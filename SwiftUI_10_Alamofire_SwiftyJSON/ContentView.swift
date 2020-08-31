0f8358c316f5e489abad826d6c2b0c04a8e3db1b
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-31 19:07:57 +0200

    [FEATURE][VIEW] View was implemented

--- SwiftUI_10_Alamofire_SwiftyJSON/ContentView.swift
+++ SwiftUI_10_Alamofire_SwiftyJSON/ContentView.swift
@@ -1,12 +1,33 @@
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ContentView: View {
     
     @ObservedObject var manager = NetworkingManager()
     
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                List (self.manager.usersList) { user in
+                    HStack {
+                        AnimatedImage(url: URL(string: user.avatar))
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .cornerRadius(50)
+                        VStack {
+                            Text(user.first_name)
+                                .font(.system(.title, design: .rounded))
+                            Text(user.last_name)
+                                .font(.system(.headline, design: .rounded))
+                            Text(user.email)
+                                .font(.system(.subheadline, design: .rounded))
+                        }
+                    }
+                    .navigationBarTitle("Alamofire", displayMode: .inline)
+                }
+            }
+        }
     }
 }
 
