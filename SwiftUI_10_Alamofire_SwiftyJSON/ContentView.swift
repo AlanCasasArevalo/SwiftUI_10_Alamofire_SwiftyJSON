37566b9c93d031678685b5411fc2e46b420bddcf
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-08-31 18:28:19 +0200

    Initial Commit

---
+++ SwiftUI_10_Alamofire_SwiftyJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_10_Alamofire_SwiftyJSON
+//
+//  Created by Everis on 31/08/2020.
+//  Copyright © 2020 Alan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
